3b67b8d1d2765af947a1d0b9abd35030dd6f96d5
projectCHOI <github.com/projectCHOI>
2024-12-18 16:17:41 +0900

    라우터

--- routes/board.js
+++ routes/board.js
@@ -1,1 +1,17 @@
 // 게시판 라우터
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/Cboard");
+
+// 자유 게시판 화면
+router.get("/", controller.showBoard);
+// 글 작성 화면
+router.get("/write", controller.showWriteForm);
+// 글 등록 처리
+router.post("/write", controller.createPost);
+// 글 확인 화면
+router.get("/view/:id", controller.showPost);
+// 의견 작성 처리
+router.post("/comment/:id", controller.createComment);
+
+module.exports = router;
