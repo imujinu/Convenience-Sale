c3ff466a42d1140302bbe7d695f912472c7973ab
imujinu <wlsdnrhdwkd@gmail.com>
2024-12-20 12:02:27 +0900

    [FRONT]메인페이지:기능개발(241220)

--- static/js/home.js
+++ static/js/home.js
@@ -103,7 +103,24 @@ container.appendChild(clone);
       method: "get",
     });
     const result = products.data.json();
-    console.log(result);
+    result.forEach((el) => {
+      const { name, price, imageUrl } = el;
+
+      const product = document.querySelector(
+        ".section5 > .container > .product ",
+      );
+      const menu = document.createElement("div");
+      menu.className = "name";
+
+      menu.innerHTML = `
+      <img src ="${imageUrl}" alt="${name}>
+      <div class="text">
+      <p> ${name}</p>
+      <p> ${price}원 </p>
+      
+      `;
+      product.append(menu);
+    });
   } catch {
     (err) => {
       console.err("err!", err);
