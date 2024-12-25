68f76b885de68ab13152ac0dbaf666a882aac2a6
imujinu <wlsdnrhdwkd@gmail.com>
2024-12-25 20:14:31 +0900

    [FRONT]main/mobile:(241225)

--- app.js
+++ app.js
@@ -58,9 +58,16 @@ app.post("/delete-account", async (req, res) => {
   });
 });
 
-db.sequelize.sync({ force: false }).then(() => {
-  console.log("DB 연결 성공");
-  app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB 연결 성공");
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+      return;
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    return;
   });
-});
