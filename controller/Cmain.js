f0278fcd6f8a6235b925592d776298048be7f99c
YENA <dr_noah@naver.com>
2024-12-27 15:48:28 +0900

    241227 회원정보수정 반영완료

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -90,15 +90,30 @@ exports.mypage = (req, res) => {
   }
 };
 
-exports.userview = (req, res) => {
+exports.userview = async (req, res) => {
   // res.render("userview", { title: "회원 수정 페이지" });
-  const user = req.session.user;
+  let user = req.session.user;
+
   if (user) {
+    const updatedUser = await models.User.findOne({
+      userId: user.userId,
+    });
+
+    // console.log(updatedUser);
+
+    if (updatedUser)
+      user = {
+        userId: updatedUser.userId,
+        userPw: updatedUser.hashedPassword,
+        nickname: updatedUser.nickname,
+        profilePath: updatedUser.profilePath,
+      };
+    req.session.user = user;
     res.render("userview", {
       name: user.nickname,
       userId: user.userId,
       nickname: user.nickname,
-      userPw: user.userPw,
+      // userPw: user.userPw,
       profilePath: user.profilePath,
       isLogin: true,
     });
